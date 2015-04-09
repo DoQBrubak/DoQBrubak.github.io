a8986b2ef52e80dcb3be171e9b94c73e7bb2107a
DoQBrubak <quintin.brubaker@gmail.com>
2015-04-09 00:08:49 -0400

    Renamed <title>; progress on NYT implement

--- js/script.js
+++ js/script.js
@@ -1,5 +1,3 @@
-var nytApiKey = '937a887a873a34ee75efafe87138b5b0:16:71810016'
-
 
 function loadData() {
 
@@ -12,22 +10,22 @@ function loadData() {
     // clear out old data before new request
     $wikiElem.text("");
     $nytElem.text("");
-    
+
+    // query streetview and use result as background image    
     var $requestStreet = $('#street').val();
     var $requestCity = $('#city').val();
-
     var imgUrl = "https://maps.googleapis.com/maps/api/streetview?location="+
         $requestStreet + ", " + $requestCity + "&size=400x400&pitch=-5&fov=90";
-
     $body.append(HTMLbgImg.replace('%data%', imgUrl));
-    console.log(imgUrl);
 
+    // query NYT Article API and use results to populate #nytimes-articles <ul>
+    var nytApiKey = '937a887a873a34ee75efafe87138b5b0:16:71810016';
+    var nytApiUrl = 'http://api.nytimes.com/svc/search/v2/articlesearch.json'+
+    '?[q=%data1%&f1=headline,web_url,snippet,abstract&sort=newest]'+
+    '&api-key='+nytApiKey;
     $.getJSON();
 
 
-    // load streetview
-
-    // YOUR CODE GOES HERE!
 
     return false;
 };
@@ -36,7 +34,6 @@ function loadUdac() {
     var apiUrl = 'https://www.udacity.com/public-api/v0/courses';
     var randDegree = Math.round(Math.random()*4);
     var randTrack = Math.round(Math.random()*6);
-    console.log('degree #: '+randDegree+'... track #: '+randTrack);
 
     $.getJSON(apiUrl, function(result){
         $('#udac-nd').html(result.degrees[randDegree]['title']);
